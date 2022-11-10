bfc5ea09f8766299b535a96ea4828b1c365191dc
anjali <anjaligit@gmail.com>
2022-11-10 08:32:55 +0530

    login css

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -34,54 +34,66 @@ const Login=()=>{
 
   return(
     <>
-     <section className="sign-in  container">
-      <div className="container mt-5">
-        <div className="signin-content   w-75 mx-auto row container my-auto">
-                  <div className="signin-image col-lg-6 col-md-6 col-12">
-                     <figure className="w-50  mx-auto img-fluid">
-                       <img className="w-100" src={login} alt="registration pic"/>
-                       </figure>
-                       <div className="image-link  my-5 text-center display-5">
-                     <NavLink to="/signup" className="signin-image-link  my-5 display-6 text-center text-capitalize" >Create an account
-                     </NavLink>
-                     </div>
-                 </div>
+{
+
+/*
+			<div class="other" id="other">
+				<div class="instead">
+					<h3>or</h3>
+				</div>
+				<button class="connect" onclick="google()">
+					<img src="images/icon/google.png"><span>Sign in with Google</span>
+				</button>
+			</div>
+     */}
+
+     <div className="form-box">
+     <div class="button-box">
+				<div id="btn"></div>
+        <NavLink to="/login" className="toggle-btn text-capitalize log " >Login </NavLink>
+        <NavLink to="/signup" className="toggle-btn text-capitalize reg"  >Register</NavLink>
+		 </div>
+			
     
-          <div className="signin-form col-lg-6 col-md-6 col-12">
-                <div className=" form-title text-center text-capitalize display-3"> sign in</div>
-              <form  method="POST" className="register-form  text-center" id="register-form">
+              <form  method="POST" className="input-group" id="register-form">
                  
-                   <div className="form-group  mx-auto mb-4">
+                   <div className="inp">
                         <label htmlFor="email">
                           <i class="zmdi zmdi-email material-icons-name"></i>
                         </label>
-                      <input type="text" name="email" id="email" 
+                      <input type="text" name="email" id="email" className="input-field"
                       value={email}
                       onChange={(e)=>setEmail(e.target.value)}
-                      autoComplete="off" placeholder="enter your email"></input>
+                      autoComplete="off" placeholder="Email"></input>
                    </div>
                    
-                   <div className="form-group mx-auto mb-4">
+                   <div className="inp">
                      <label htmlFor="password">
                       <i class="zmdi zmdi-lock material-icons-name"></i>
                      </label>
-                     <input type="password" name="password" id="password" autoComplete="off"
+                     <input type="password" name="password" id="password" autoComplete="off" className="input-field"  
                      value={password}
                      onChange={(e)=>setPassword(e.target.value)}
-                     placeholder="enter your password"></input>
+                     placeholder="Password"></input>
                    </div>
                    
-                   <div className="form-group btn  pt-4 form-button">
-                       <input type="submit" name="signin" id="signin" 
+                   <div className="submit-btn">
+                       <input type="submit" name="signin" 
                        onClick={loginUser}
-                       className="form-submit" value="Log in"/>
+                       className="btn" value="Log in"/>
                    </div>
              </form>
-         </div>
-       </div>
-     </div>
-  </section>  
- </>
+
+             <div className="other" id="other">
+				<div className="instead">
+					<h3>or</h3>
+				</div>
+				<button className="connect" onclick="google()">
+					<span>Sign in with Google</span>
+				</button>
+			</div>
+      </div>
+  </>
   );
 }
 export default Login;
