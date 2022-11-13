7c8fea64de92a9cf57564cc3ce4f7678e0cdc740
anjali <anjaligit@gmail.com>
2022-11-13 15:44:08 +0530

    categoryPart

---
+++ client/src/components/Coding.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Contact from './Contact.js';
+import Navbar from "./Navbar.js";
+const Coding=()=> {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Coding;
