7725ccd03e99ed46e843c6c3879e8f48ea29f9a6
apurvk4 <apurvkumar4299@gmail.com>
2022-11-23 18:55:04 +0530

    updated cors setting

--- api/index.js
+++ api/index.js
@@ -11,7 +11,10 @@ const jwt = require("jsonwebtoken");
 const User = require("./model/userSchema");
 const Admin = require("./model/adminSchema");
 function corsMiddleWare(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://techno-placement11.vercel.app/"
+  );
   res.removeHeader("X-powered-by");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PATCH,POST,DELETE");
   res.setHeader("Access-Control-Allow-Credentials", "true");
