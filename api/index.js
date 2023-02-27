1e11d363108968dce2d7f6d97f9a3bbdaec86aff
apurvk4 <apurvkumar4299@gmail.com>
2023-02-27 22:21:31 +0530

    cors updated

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const jwt = require("jsonwebtoken");
 const User = require("./model/userSchema");
 const Admin = require("./model/adminSchema");
 function corsMiddleWare(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", process.env.ALLOWED);
   res.removeHeader("X-powered-by");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE");
   res.setHeader("Access-Control-Allow-Credentials", "true");
