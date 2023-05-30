42d445a415c650da898ba21ced8f5128a23dabb3
apurvk4 <apurvkumar4299@gmail.com>
2023-05-30 13:58:44 +0530

    updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,26 +7,38 @@ import location from "../images/location.png";
 const Footer=()=> {
   return (
     <>
-    <div className='footer'>
-       <div className="footer-container">
-			<div className="left-col">
-			<h1 >Our Newsletter</h1>
-				<img src={logo}  style={{width:"200px"}} />
-				<h3  className="  m-0" style={{fontFamily:"monospace"}}>Techno Placement</h3>
-				<div className="logo"></div>
-				
-			</div>
-			<div className="right-col mb-0">
-				
-				<p className="rights-text">Copyright © 2022 Created By Apurv Kumar,Anjali Jain, Ankit Mathur,Bharat Kumar. All Rights Reserved.</p>
-				<div className="border"></div><br/>
-				<p><img src={phone}/> +91-1234-567-777<br/></p>
-				<br/><p><img src={location}/>SKIT, Jaipur<br/>Jaipur, Rajasthan</p><br/>
-				
-			</div>
-		</div>
+      <div className="footer">
+        <div className="footer-container">
+          <div className="left-col">
+            <h1>Our Newsletter</h1>
+            <img src={logo} style={{ width: "200px" }} />
+            <h3 className="  m-0" style={{ fontFamily: "monospace" }}>
+              Techno Placement
+            </h3>
+            <div className="logo"></div>
+          </div>
+          <div className="right-col mb-0">
+            <p className="rights-text">
+              Copyright © 2023 Created By Apurv Kumar,Anjali Jain, Ankit
+              Mathur,Bharat Kumar. All Rights Reserved.
+            </p>
+            <div className="border"></div>
+            <br />
+            <p>
+              <img src={phone} /> +91-1234-567-777
+              <br />
+            </p>
+            <br />
+            <p>
+              <img src={location} />
+              SKIT, Jaipur
+              <br />
+              Jaipur, Rajasthan
+            </p>
+            <br />
+          </div>
         </div>
-      
+      </div>
     </>
   );
 };
